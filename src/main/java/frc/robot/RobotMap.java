0abfa1d5cf97b9df0b466f4f10e6b4c31f59d5f0
Tal_Sitton <tal.sitton04@gmail.com>
2019-07-25 13:40:48 +0300

    added all subsystems

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -7,6 +7,8 @@
 
 package frc.robot;
 
+import frc.robot.subsystems.Gripper;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -14,13 +16,24 @@ package frc.robot;
  * floating around.
  */
 public class RobotMap {
-  // For example to map the left and right motors, you could define the
-  // following variables to use with your drivetrain subsystem.
-  // public static int leftMotor = 1;
-  // public static int rightMotor = 2;
+    public static interface PWM {
+        public static final int GRIPPER_MOTOR = 4;
+        public static final int DRIVE_TRAIN_LEFT_FRONT = 3;
+        public static final int DRIVE_TRAIN_LEFT_BACK = 2;
+        public static final int DRIVE_TRAIN_RIGHT_FRONT = 1;
+        public static final int DRIVE_TRAIN_RIGHT_BACK = 0;
+    }
 
-  // If you are using multiple modules, make sure to define both the port
-  // number and the module. For example you with a rangefinder:
-  // public static int rangefinderPort = 1;
-  // public static int rangefinderModule = 1;
+    public static interface DIO {
+        public static final int GRIPPER_LIMIT_SWITCH = 8;
+    }
+    // For example to map the left and right motors, you could define the
+    // following variables to use with your drivetrain subsystem.
+    // public static int leftMotor = 1;
+    // public static int rightMotor = 2;
+
+    // If you are using multiple modules, make sure to define both the port
+    // number and the module. For example you with a rangefinder:
+    // public static int rangefinderPort = 1;
+    // public static int rangefinderModule = 1;
 }
